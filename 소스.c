02139c58e8afefdbf3de1b0242ed3018771cc598
unknown <hs.davidlewis@gmail.com>
2023-01-07 15:44:37 +0900

    let there be light

---
+++ 소스.c
@@ -0,0 +1,46 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+	int ch1, ch2;
+	ch1 = getchar();
+	ch2 = fgetc(stdin);
+	
+	putchar(ch1);
+	fputc(ch2, stdout);
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main()
+{
+	int ch;
+	
+	while (1)
+	{
+		ch = getchar();
+		if (ch == EOF)
+			break;
+		putchar(ch);
+	}
+	return 0;
+}
+*/
+
+#include <stdio.h>
+
+int index(chi)
+{
+	if()
+	if()
+}
+
+int main()
+{
+	int ch;
+	ch = getchar();
+	index(&ch);
+}
